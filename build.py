a3ee0f35a55b0eba3ac106e610b4558aceb28219
Uilian Ries <uilianries@gmail.com>
2018-08-12 12:01:25 -0300

    #399 Add fmt ranges testing
    - Validate more option using fmt package
    - Remove cppstd from settings (was removed on version 5.1.0)
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -8,12 +8,10 @@ import os
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_default.get_builder(pure_c=False)
 
     # Add one extra build to create a header-only version
     if platform.system() == "Linux" and os.getenv("CONAN_GCC_VERSIONS", False) == "6.3":
         builder.add({}, {"fmt:header_only" : True}, {}, {})
-    
-    builder.run()
 
-    
+    builder.run()
