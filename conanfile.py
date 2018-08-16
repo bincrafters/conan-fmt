c7deff2b48ad2bb85951dc8c389bef93724e2a37
Uilian Ries <uilianries@gmail.com>
2018-08-16 16:06:20 -0300

    #399 run find_package fmt on test_package
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,6 @@
 
 from conans import ConanFile, CMake, tools
 import os
-import shutil
 
 
 class FmtConan(ConanFile):
@@ -63,10 +62,6 @@ class FmtConan(ConanFile):
         else:
             cmake = self.configure_cmake()
             cmake.install()
-            if self.settings.os == "Windows" and self.options.shared:
-                src_path = os.path.join(self.package_folder, "lib", "fmt.dll")
-                dst_path = os.path.join(self.package_folder, "bin", "fmt.dll")
-                shutil.move(src_path, dst_path)
 
     def package_info(self):
         if self.options.header_only:
